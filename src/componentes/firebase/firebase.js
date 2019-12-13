c4e4a87478ff8e6330888b859dfc1e45714feb2f
NatThome <nathythome05@gmail.com>
2019-12-13 12:33:22 -0300

    arquivos instalados

---
+++ src/componentes/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKY_nZDBldN9M0YitGUEtxmmEmnI2KEcw",
+    authDomain: "burger-queen-8c4ba.firebaseapp.com",
+    databaseURL: "https://burger-queen-8c4ba.firebaseio.com",
+    projectId: "burger-queen-8c4ba",
+    storageBucket: "burger-queen-8c4ba.appspot.com",
+    messagingSenderId: "288460885507",
+    appId: "1:288460885507:web:588dfc0edb89d12a0a0836",
+    measurementId: "G-LS2VXNXXL4"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp;
